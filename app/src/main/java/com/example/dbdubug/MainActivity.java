72e39710e590c4ce8bc4a9e435e1ebefff178ef4
zhaohw <525124094@qq.com>
2020-07-23 22:36:09 +0800

    <>commit

--- app/src/main/java/com/example/dbdubug/MainActivity.java
+++ app/src/main/java/com/example/dbdubug/MainActivity.java
@@ -46,84 +46,89 @@ public class MainActivity extends AppCompatActivity {
 		handlerThread.start();
 		mHandler = new Handler(handlerThread.getLooper());
 		
-		Set<String> stringSet = new HashSet<>();
-		stringSet.add("SetOne");
-		stringSet.add("SetTwo");
-		stringSet.add("SetThree");
-		
-		SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
-		
-		SharedPreferences prefsOne = getSharedPreferences("countPrefOne", Context.MODE_PRIVATE);
-		SharedPreferences prefsTwo = getSharedPreferences("countPrefTwo", Context.MODE_PRIVATE);
-		
-		sharedPreferences.edit().putString("testOne", "one").commit();
-		sharedPreferences.edit().putInt("testTwo", 2).commit();
-		sharedPreferences.edit().putLong("testThree", 100000L).commit();
-		sharedPreferences.edit().putFloat("testFour", 3.01F).commit();
-		sharedPreferences.edit().putBoolean("testFive", true).commit();
-		sharedPreferences.edit().putStringSet("testSix", stringSet).commit();
-		
-		prefsOne.edit().putString("testOneNew", "one").commit();
-		
-		prefsTwo.edit().putString("testTwoNew", "two").commit();
-		
-		ContactDBHelper contactDBHelper = new ContactDBHelper(getApplicationContext());
-		if (contactDBHelper.count() == 0) {
-			for (int i = 0; i < 100; i++) {
-				String name = "name_" + i;
-				String phone = "phone_" + i;
-				String email = "email_" + i;
-				String street = "street_" + i;
-				String place = "place_" + i;
-				contactDBHelper.insertContact(name, phone, email, street, place);
+		mHandler.post(new Runnable() {
+			@Override
+			public void run() {
+				Set<String> stringSet = new HashSet<>();
+				stringSet.add("SetOne");
+				stringSet.add("SetTwo");
+				stringSet.add("SetThree");
+				
+				SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+				
+				SharedPreferences prefsOne = getSharedPreferences("countPrefOne", Context.MODE_PRIVATE);
+				SharedPreferences prefsTwo = getSharedPreferences("countPrefTwo", Context.MODE_PRIVATE);
+				
+				sharedPreferences.edit().putString("testOne", "one").commit();
+				sharedPreferences.edit().putInt("testTwo", 2).commit();
+				sharedPreferences.edit().putLong("testThree", 100000L).commit();
+				sharedPreferences.edit().putFloat("testFour", 3.01F).commit();
+				sharedPreferences.edit().putBoolean("testFive", true).commit();
+				sharedPreferences.edit().putStringSet("testSix", stringSet).commit();
+				
+				prefsOne.edit().putString("testOneNew", "one").commit();
+				
+				prefsTwo.edit().putString("testTwoNew", "two").commit();
+				
+				ContactDBHelper contactDBHelper = new ContactDBHelper(getApplicationContext());
+				if (contactDBHelper.count() == 0) {
+					for (int i = 0; i < 100; i++) {
+						String name = "name_" + i;
+						String phone = "phone_" + i;
+						String email = "email_" + i;
+						String street = "street_" + i;
+						String place = "place_" + i;
+						contactDBHelper.insertContact(name, phone, email, street, place);
+					}
+				}
+				
+				CarDBHelper carDBHelper = new CarDBHelper(getApplicationContext());
+				if (carDBHelper.count() == 0) {
+					for (int i = 0; i < 50; i++) {
+						String name = "name_" + i;
+						String color = "RED";
+						float mileage = i + 10.45f;
+						carDBHelper.insertCar(name, color, mileage);
+					}
+				}
+				
+				ExtTestDBHelper extTestDBHelper = new ExtTestDBHelper(getApplicationContext());
+				if (extTestDBHelper.count() == 0) {
+					for (int i = 0; i < 20; i++) {
+						String value = "value_" + i;
+						extTestDBHelper.insertTest(value);
+					}
+				}
+				
+				// Room database
+				userDBHelper = new UserDBHelper(getApplicationContext());
+				if (userDBHelper.count() == 0) {
+					List<User> userList = new ArrayList<>();
+					for (int i = 0; i < 5; i++) {
+						User user = new User();
+						user.id = (long)(i + 1);
+						user.name = "user_" + i;
+						userList.add(user);
+					}
+					userDBHelper.insertUser(userList);
+				}
+				
+				// Room inMemory database
+				if (userDBHelper.countInMemory() == 0) {
+					List<User> userList = new ArrayList<>();
+					for (int i = 0; i < 5; i++) {
+						User user = new User();
+						user.id = (long)(i + 1);
+						user.name = "in_memory_user_" + i;
+						userList.add(user);
+					}
+					userDBHelper.insertUserInMemory(userList);
+				}
+				
+				Utils.setCustomDatabaseFiles(getApplicationContext());
+				Utils.setInMemoryRoomDatabases(userDBHelper.getInMemoryDatabase());
 			}
-		}
-		
-		CarDBHelper carDBHelper = new CarDBHelper(getApplicationContext());
-		if (carDBHelper.count() == 0) {
-			for (int i = 0; i < 50; i++) {
-				String name = "name_" + i;
-				String color = "RED";
-				float mileage = i + 10.45f;
-				carDBHelper.insertCar(name, color, mileage);
-			}
-		}
-		
-		ExtTestDBHelper extTestDBHelper = new ExtTestDBHelper(getApplicationContext());
-		if (extTestDBHelper.count() == 0) {
-			for (int i = 0; i < 20; i++) {
-				String value = "value_" + i;
-				extTestDBHelper.insertTest(value);
-			}
-		}
-		
-		// Room database
-		userDBHelper = new UserDBHelper(getApplicationContext());
-		if (userDBHelper.count() == 0) {
-			List<User> userList = new ArrayList<>();
-			for (int i = 0; i < 5; i++) {
-				User user = new User();
-				user.id = (long)(i + 1);
-				user.name = "user_" + i;
-				userList.add(user);
-			}
-			userDBHelper.insertUser(userList);
-		}
-		
-		// Room inMemory database
-		if (userDBHelper.countInMemory() == 0) {
-			List<User> userList = new ArrayList<>();
-			for (int i = 0; i < 5; i++) {
-				User user = new User();
-				user.id = (long)(i + 1);
-				user.name = "in_memory_user_" + i;
-				userList.add(user);
-			}
-			userDBHelper.insertUserInMemory(userList);
-		}
-		
-		Utils.setCustomDatabaseFiles(getApplicationContext());
-		Utils.setInMemoryRoomDatabases(userDBHelper.getInMemoryDatabase());
+		});
 	}
 	
 	public void click(View view) {
